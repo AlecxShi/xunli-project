ddf152c94c490e605a652fd66125d9796b89ea46
AlecxShi <shihuijiang@yeah.net>
2017-09-04 16:56:11 +0800

    字典给前端取值

---
+++ src/main/java/com/xunli/manager/web/DictInfo2AppController.java
@@ -0,0 +1,36 @@
+package com.xunli.manager.web;
+
+import com.xunli.manager.enumeration.ReturnCode;
+import com.xunli.manager.model.DictInfo;
+import com.xunli.manager.model.RequestResult;
+import com.xunli.manager.repository.DictInfoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by shihj on 2017/9/4.
+ */
+@RestController
+@RequestMapping("/api/dict")
+public class DictInfo2AppController {
+
+    @Autowired
+    private DictInfoRepository dictInfoRepository;
+
+    @RequestMapping(value = "/query",method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
+    public RequestResult getDictInfoByType(String type)
+    {
+        List<DictInfo> result = new ArrayList();
+        if(type != null)
+        {
+            result = dictInfoRepository.findAllByDictType(type);
+        }
+        return new RequestResult(ReturnCode.PUBLIC_SUCCESS,result);
+    }
+}
