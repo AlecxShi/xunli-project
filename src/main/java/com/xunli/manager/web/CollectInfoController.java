db482d005763531c2068cbcb7e422a3c6a0c339c
AlecxShi <shihuijiang@yeah.net>
2017-09-07 13:20:35 +0800

    new commit

--- src/main/java/com/xunli/manager/web/CollectInfoController.java
+++ src/main/java/com/xunli/manager/web/CollectInfoController.java
@@ -1,9 +1,11 @@
 package com.xunli.manager.web;
 
 import com.xunli.manager.enumeration.ReturnCode;
+import com.xunli.manager.model.ChildrenInfo;
 import com.xunli.manager.model.CommonUserLogins;
 import com.xunli.manager.model.RequestResult;
 import com.xunli.manager.model.UserCollectInfo;
+import com.xunli.manager.repository.ChildrenInfoRepository;
 import com.xunli.manager.repository.CommonUserLoginsRepository;
 import com.xunli.manager.repository.UserCollectInfoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
-import java.util.Optional;
+import java.util.*;
 
 /**
  * Created by shihj on 2017/9/7.
@@ -29,6 +30,9 @@ public class CollectInfoController {
     @Autowired
     private UserCollectInfoRepository userCollectInfoRepository;
 
+    @Autowired
+    private ChildrenInfoRepository childrenInfoRepository;
+
     @RequestMapping(value = "/collect/save",method = RequestMethod.POST,produces = MediaType.APPLICATION_JSON_VALUE)
     @Transactional
     public RequestResult save(String token,Long targetUserId)
@@ -38,7 +42,7 @@ public class CollectInfoController {
             return new RequestResult(ReturnCode.PUBLIC_PARAMETER_MISSING);
         }
         CommonUserLogins login = commonUserLoginsRepository.getByToken(token);
-        if(login.getExpireTime().compareTo(new Date()) <= 0)
+        if(login == null || login.getExpireTime().compareTo(new Date()) <= 0)
         {
             return new RequestResult(ReturnCode.PUBLIC_TOKEN_IS_INVALID);
         }
@@ -52,5 +56,42 @@ public class CollectInfoController {
             userCollectInfoRepository.save(collectInfo);
             return new RequestResult(ReturnCode.PUBLIC_SUCCESS);
         });
+    }
+
+    @RequestMapping(value = "/collect/getAll",method = RequestMethod.POST,produces = MediaType.APPLICATION_JSON_VALUE)
+    @Transactional(readOnly = true)
+    public RequestResult getAll(String token)
+    {
+        if(token == null)
+        {
+            return new RequestResult(ReturnCode.PUBLIC_PARAMETER_MISSING);
+        }
+        CommonUserLogins login = commonUserLoginsRepository.getByToken(token);
+        if(login == null || login.getExpireTime().compareTo(new Date()) <= 0)
+        {
+            return new RequestResult(ReturnCode.PUBLIC_TOKEN_IS_INVALID);
+        }
+        List<Long> parentIds = new ArrayList<>();
+        for(UserCollectInfo collect : userCollectInfoRepository.findAllByUserId(login.getUserId()))
+        {
+            parentIds.add(collect.getTargetUserId());
+        }
+        List<Map<String,Object>> list = new ArrayList<Map<String,Object>>();
+        if(!parentIds.isEmpty())
+        {
+            for(ChildrenInfo info : childrenInfoRepository.findAllByParentIdIn(parentIds))
+            {
+                Map<String,Object> data = new HashMap();
+                data.put("userId",info.getParentId());
+                data.put("name",info.getName());
+                data.put("bornLocation",info.getBornLocation());
+                data.put("currentLocation",info.getCurrentLocation());
+                data.put("birthday",info.getBirthday());
+                data.put("education",info.getEducation());
+                data.put("label",info.getLabel());
+                list.add(data);
+            }
+        }
+        return new RequestResult(ReturnCode.PUBLIC_SUCCESS,list);
     }
 }
