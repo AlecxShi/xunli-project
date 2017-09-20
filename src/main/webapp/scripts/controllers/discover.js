3f0e27f5b62929a674d2a6d1390ffa419e25dfd7
AlecxShi <shihuijiang@yeah.net>
2017-09-20 17:26:14 +0800

    commit

--- src/main/webapp/scripts/controllers/discover.js
+++ src/main/webapp/scripts/controllers/discover.js
@@ -130,20 +130,83 @@ angular.module('app')
       $scope.refresh();
     });
 })
-.controller('DiscoverEditController',function($scope, $mdToast,$mdDialog,DictInfo,article) {
-
-	  $scope.dict = article;
+.controller('DiscoverEditController',function($q,$state,$scope, $mdToast,$mdDialog,DictInfo,article,Discover) {
+	  $scope.article = article || {};
 	  $scope.error = "";
+	  $scope.article.content = "shihuijiang";
 	  $scope.title = article.id === 'new' ? "添加新字典" : "修改字典";
+	  $scope.config =
+	  {
+            //是否聚焦 focus默认为false
+            focus : true,
+            //首行缩进距离,默认是2em
+            indentValue:'2em',
+            //初始化编辑器宽度,默认100%
+            initialFrameWidth:'100%',
+            //初始化编辑器高度,默认320
+            initialFrameHeight:550,
+            //编辑器初始化结束后,编辑区域是否是只读的，默认是false
+            readonly : false ,
+            //启用自动保存
+            enableAutoSave: false,
+            //自动保存间隔时间， 单位ms
+            saveInterval: 3000,
+            //是否开启初始化时即全屏，默认关闭
+            fullscreen : false,
+            //图片操作的浮层开关，默认打开
+            imagePopup:true,
+            //提交到后台的数据是否包含整个html字符串
+            allHtmlEnabled:false,
+            //额外功能添加
+            functions :['map','insertimage','insertvideo','attachment','insertcode','webapp','template','background','wordimage']
+      };
 	  $scope.hide = function() {
 	     $mdDialog.hide();
 	  };
+
+      $scope.getFile = function () {
+          $scope.readAsDataURL($scope.articleIcon, $scope)
+            .then(function(result) {
+                $scope.imageSrc = result;
+            });
+      };
+
+      $scope.onLoad = function(reader, deferred, scope) {
+         return function () {
+             scope.$apply(function () {
+                 deferred.resolve(reader.result);
+             });
+         };
+      };
+
+      $scope.onError = function (reader, deferred, scope) {
+         return function () {
+             scope.$apply(function () {
+                 deferred.reject(reader.result);
+             });
+         };
+      };
+
+      $scope.getReader = function(deferred, scope) {
+          var reader = new FileReader();
+          reader.onload = $scope.onLoad(reader, deferred, scope);
+          reader.onerror = $scope.onError(reader, deferred, scope);
+          return reader;
+      };
+
+      $scope.readAsDataURL = function (file, scope) {
+          var deferred = $q.defer();
+          var reader = $scope.getReader(deferred, scope);
+          reader.readAsDataURL(file);
+          return deferred.promise;
+      };
 
 	  $scope.cancel = function() {
 	     $mdDialog.cancel();
 	  };
 
 	  $scope.save = function(ev){
+	      console.log($scope.article);
 		  $scope.error = "";
 		  event.preventDefault();
 	      angular.forEach($scope.form.$error.required, function(field) {
@@ -152,7 +215,7 @@ angular.module('app')
 
 	      if ($scope.form.$valid)
           {
-             DictInfo.save($scope.dict).$promise.then(
+             Discover.save($scope.dict).$promise.then(
                     function(result, responseHeaders) {
                         $scope.error = null;
                         $scope.success = 'OK';
@@ -177,4 +240,9 @@ angular.module('app')
              $mdDialog.hide();
           }
 	  };
+
+	  $scope.cancel = function()
+	  {
+	    $state.go('discover.list');
+	  }
 })
