8e64da2f34c206edd12835b9e13ee850fc8bd9f1
AlecxShi <shihuijiang@yeah.net>
2017-09-29 11:08:11 +0800

    commit

--- src/main/java/com/xunli/manager/repository/CommonUserRepository.java
+++ src/main/java/com/xunli/manager/repository/CommonUserRepository.java
@@ -3,6 +3,9 @@ package com.xunli.manager.repository;
 import com.xunli.manager.model.CommonUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,4 +18,11 @@ public interface CommonUserRepository extends JpaRepository<CommonUser, Long>, J
     List<CommonUser> findAllByIdIn(List<Long> ids);
 
     Optional<CommonUser> findOneByPhone(String value);
+
+    List<CommonUser> findAllByPasswordIsNull();
+
+    @Query("UPDATE CommonUser SET password = ?1 WHERE password IS NULL")
+    @Modifying
+    @Transactional
+    void updatePassword(String password);
 }
